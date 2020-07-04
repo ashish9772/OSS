4c2ce06eb9a05e0d054bc3d9c343b787417e384b
Rajesh <srrajesh.svpur@gmail.com>
2020-07-04 23:48:57 +0530

    service intergation

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -30,8 +30,7 @@ export class ApiService {
   post(path: string, body: Object = {}): Observable<any> {
     return this.http.post(
       `${environment.api_url}${path}`,
-      JSON.stringify(body),
-      { headers: { "Access-Control-Allow-Origin" : "*" } }
+      JSON.stringify(body)
     ).pipe(catchError(this.formatErrors));
   }
 
